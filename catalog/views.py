253705dd1b989719d9da6b744ab9ef5cf1d96c9e
Вячеслав Лукин <bafana.slv@gmail.com>
2024-05-30 20:43:15 +0300

    Commit № 2

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'contacts.html')
