33ec6d94c54d06b70c8aacc2104a5d187fa30058
Вячеслав Лукин <bafana.slv@gmail.com>
2024-06-05 19:59:05 +0300

    Подключение к БД

--- config/settings.py
+++ config/settings.py
@@ -51,8 +51,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_shop',
+        'USER': 'postgres',
+        'PASSWORD': '12345'
     }
 }
 
