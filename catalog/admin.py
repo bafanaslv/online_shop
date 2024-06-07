41c8dc908409269ed2b88a8b954da8efea1fdbd0
Вячеслав Лукин <bafana.slv@gmail.com>
2024-06-07 15:52:18 +0300

    Миграция 0002

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from catalog.models import Category, Products
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
--- catalog/models.py
+++ catalog/models.py
@@ -23,11 +23,10 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', help_text='Введите цену продукта')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    manufactured_at = models.DateField(default=datetime.date.today())
 
     def __str__(self):
         return f'{self.name} {self.description} {self.price}'
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
