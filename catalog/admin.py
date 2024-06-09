050f0ee03d40d6aa5e026e3a603a30991ee34f51
Вячеслав Лукин <bafana.slv@gmail.com>
2024-06-09 05:24:07 +0300

    Init

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,6 +9,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category")
+    list_display = ("id", "name", "price", "category",)
     list_filter = ("category",)
-    search_fields = ("name", "description")
+    search_fields = ("name", "description",)
