d39c0d6668962f526b38b067ad74f27e30b3ed49
Вячеслав Лукин <bafana.slv@gmail.com>
2024-06-28 18:33:09 +0300

    Добавление кнопок для промотрпа отдельных категорий.

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', views.products_list, name='products_list'),
     path('catagory/<int:cat>', views.products_category, name='products_category'),
-    path('catalog/<int:pk>/', views.product_detail, name='product_detail'),
+    path('catalog/<int:pk>/', views.product, name='product_detail'),
     path('contact/', views.contact, name='contact')
 ]
