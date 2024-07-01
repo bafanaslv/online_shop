b086697a127f503a13b4cb51af4899c14d01fabf
Вячеслав Лукин <bafana.slv@gmail.com>
2024-07-01 20:27:36 +0300

    Созданеие приложения Blog

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='list'),
-    # path('category/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('create/', views.BlogCreateView.as_view(), name='create'),
-    # path('category/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
-    # path('category/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete')
+    path('<int:pk>/update/', views.BlogUpdateView.as_view(), name='create'),
+    path('<int:pk>/delete/', views.BlogDeleteView.as_view(), name='confirm_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from blog.models import Blog
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 
 
@@ -10,5 +10,17 @@ class BlogCreateView(CreateView):
     success_url = reverse_lazy("catalog:products_list")
 
 
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ("title", "body")
+    success_url = reverse_lazy("catalog:products_list")
+
+
 class BlogListView(ListView):
     model = Blog
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    # template_name = 'blog/confirm_delete.html'
+    success_url = reverse_lazy("catalog:products_list")
