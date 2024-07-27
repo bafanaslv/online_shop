01a78e46fb5d12a403e27108e859c6fe7a11d54d
Вячеслав Лукин <bafana.slv@gmail.com>
2024-07-27 07:29:47 +0300

    Выполнение ДЗ №6

--- catalog/services.py
+++ catalog/services.py
@@ -1,10 +1,10 @@
 from django.core.cache import cache
-from catalog.models import Products
+from catalog.models import Products, Category
 from config.settings import CACHE_ENABLED
 
 
 def get_products_from_cache():
-    """Получаем данные из кэша если он не пустой и из базы данных если пустой."""
+    """Получаем список продуктов из кэша если он не пустой илил из базы данных если пустой."""
     if not CACHE_ENABLED:
         return Products.objects.all()
     key = "products_list"
@@ -14,3 +14,16 @@ def get_products_from_cache():
     products = Products.objects.all()
     cache.set(key, products)
     return products
+
+
+def get_catalog_from_cache():
+    """Получаем список категорий из кэша если он не пустой или из базы данных если пустой."""
+    if not CACHE_ENABLED:
+        return Category.objects.all()
+    key = "categories_list"
+    categories = cache.get(key)
+    if categories is not None:
+        return categories
+    categories = Category.objects.all()
+    cache.set(key, categories)
+    return categories
